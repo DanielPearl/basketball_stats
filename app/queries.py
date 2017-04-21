6da300a8590cb62b79179e3890189b25de22c04b
Danielpearl <danielaaronpearl@gmail.com>
2017-04-21 11:42:52 -0700

    compared winner of matches to sum of specific stats to determine probability of winning

--- app/queries.py
+++ app/queries.py
@@ -39,45 +39,45 @@ def get_stats_dictionary():
     """
     Returns dictionary of team stats
     """
-    return {'games_played': 0, 'minutes_played': 0, 'points': 0, 'o_rebounds': 0, 'd_rebounds': 0, 'assists': 0, 'steals': 0, 'blocks': 0, 'turnovers': 0, 'personal_fouls': 0, 'field_goal_attempts': 0, 'field_goals': 0, 'free_throw_attempts': 0, 'free_throws': 0, 'three_point_attempts': 0, 'three_points': 0}
+    return {'team_id':0, 'games_played': 0, 'minutes_played': 0, 'points': 0, 'o_rebounds': 0, 'd_rebounds': 0, 'assists': 0, 'steals': 0, 'blocks': 0, 'turnovers': 0, 'personal_fouls': 0, 'field_goal_attempts': 0, 'field_goals': 0, 'free_throw_attempts': 0, 'free_throws': 0, 'three_point_attempts': 0, 'three_points': 0}
 
 def get_team_stats(team, year):
     """
     Selects stats on a team during a specific year
     """
-    stats = session.query(Player).filter(Player.team_id == team).filter(Player.year == year).all()
+    stats = session.query(Player).filter(Player.team_id == team).filter(Player.year == year).filter(Player.games_played != 0).all()
 
     team_dict = get_stats_dictionary()
 
-    if stats != None:
+    if stats != None and stats != 0:
         for stat in stats:
-            team_dict['games_played'] += stat.games_played
-            team_dict['minutes_played'] += stat.minutes_played
-            team_dict['points'] += stat.points
-            team_dict['o_rebounds'] += stat.o_rebounds
-            team_dict['d_rebounds'] += stat.d_rebounds
-            team_dict['assists'] += stat.assists
-            team_dict['steals'] += stat.steals
-            team_dict['blocks'] += stat.blocks
-            team_dict['turnovers'] += stat.turnovers
-            team_dict['personal_fouls'] += stat.personal_fouls
-            team_dict['field_goal_attempts'] += stat.field_goal_attempts
-            team_dict['field_goals'] += stat.field_goals
-            team_dict['free_throw_attempts'] += stat.free_throw_attempts
-            team_dict['free_throws'] += stat.free_throws
-            team_dict['three_point_attempts'] += stat.three_point_attempts
-            team_dict['three_points'] += stat.three_points
+                team_dict['team_id'] += stat.games_played
+                team_dict['games_played'] += stat.games_played
+                team_dict['minutes_played'] += stat.minutes_played
+                team_dict['points'] += stat.points
+                team_dict['o_rebounds'] += stat.o_rebounds
+                team_dict['d_rebounds'] += stat.d_rebounds
+                team_dict['assists'] += stat.assists
+                team_dict['steals'] += stat.steals
+                team_dict['blocks'] += stat.blocks
+                team_dict['turnovers'] += stat.turnovers
+                # team_dict['field_goal_attempts'] += stat.field_goal_attempts
+                team_dict['field_goals'] += stat.field_goals
+                # team_dict['free_throw_attempts'] += stat.free_throw_attempts
+                team_dict['free_throws'] += stat.free_throws
+                # team_dict['three_point_attempts'] += stat.three_point_attempts
+                team_dict['three_points'] += stat.three_points
     return team_dict
 
-def get_game_results(team1, team2, year):
-    team1_results = session.query(Series).filter(Series.winner == team1, Series.loser == team2).filter(Series.year == year).all()
-    team2_results = session.query(Series).filter(Series.winner == team2, Series.loser == team1).filter(Series.year == year).all()
+def get_game_results():
+    results = session.query(Series.winner, Series.loser, Series.year)
+    result_dict = {'predicted':0, 'total':0}
 
-    result_dict = {"{}".format(team1):0, "{}".format(team2):0}
-    if team1_results != None:
-        for result in team1_results:
-            result_dict[team1] += result.games_won
-        for result in team2_results:
-            result_dict[team2] += result.games_won
-
+    for result in results:
+        winner_stats = sum(get_team_stats(result.winner, result.year).values())
+        loser_stats = sum(get_team_stats(result.loser, result.year).values())
+        if winner_stats != 0 and loser_stats != 0:
+            if winner_stats > loser_stats:
+                result_dict['predicted'] += 1
+            result_dict['total'] += 1
     return result_dict
--- app/routes.py
+++ app/routes.py
@@ -8,7 +8,7 @@ from .models import *
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = forms.TeamForm(request.form, csrf_enabled=True)
-    sum1, sum2, team1_wins, team2_wins, results = None, None, None, None, None
+    sum1, sum2, team1_wins, team2_wins, results, stats, probability = None, None, None, None, None, None, None
     if request.method == 'POST':
         team1 = form.team1.data
         team2 = form.team2.data
@@ -22,7 +22,7 @@ def index():
         sum2 = sum(stats2.values())
         stats = {team1: sum1, team2: sum2}
 
-        results = queries.get_game_results(team1, team2, year)
-
-    context = {'form':form, 'total':stats, 'results':results}
+        results = queries.get_game_results()
+        probability = round(100 * (results['predicted']/results['total']), 2)
+    context = {'form':form, 'total':stats, 'probability':probability}
     return render_template('index.html', context=context)
